db5b6334de329556f6c966f1429ea5a4c994b44e
FireFlyDeveloper <saludeskimdev@gmail.com>
2025-03-01 12:20:34 +0800

    Add admin routes for managing teachers, students, courses, departments, and classes

--- src/route/router.ts
+++ src/route/router.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import login from "./login";
+import admin from "./admin";
 
 const router = new Hono();
 
@@ -14,5 +15,25 @@ router.post("login-teacher", (c) => login.teachers_api(c));
 // Root Login
 router.get("login-admin", (c) => login.admin_login(c));
 router.post("login-admin", (c) => login.admin_api(c));
+
+// ############## Admin Routes ############## //
+router.get("teachers", (c) => admin.getAllTeachers(c));
+router.post("teachers", (c) => admin.createTeacher(c));
+
+// Student Routes
+router.get("students", (c) => admin.getAllStudents(c));
+router.post("students", (c) => admin.createStudent(c));
+
+// Course Routes
+router.get("courses", (c) => admin.getAllCourses(c));
+router.post("courses", (c) => admin.createCourse(c));
+
+// Department Routes
+router.get("departments", (c) => admin.getAllDepartments(c));
+router.post("departments", (c) => admin.createDepartment(c));
+
+// Class Routes
+router.get("classes", (c) => admin.getAllClasses(c));
+router.post("classes", (c) => admin.createClass(c));
 
 export default router;
